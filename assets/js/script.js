97af761f0d5d3ad7a2a3846ba6529238aa8177ea
Eileen Harvey <98053169+eileenmh@users.noreply.github.com>
2023-02-17 17:37:43 -0500

    create timer that runs on button click

--- assets/js/script.js
+++ assets/js/script.js
@@ -1,1 +1,14 @@
 var startButtonEl = document.querySelector("#start-button");
+var timerEl = document.querySelector("#timer");
+
+var timeLeft = 60;
+
+function runTimer() {
+    setInterval(countdown, 1000);
+    function countdown() {
+        timeLeft--;
+        timerEl.innerHTML = timeLeft;
+    }
+}
+
+startButtonEl.addEventListener('click', runTimer);
